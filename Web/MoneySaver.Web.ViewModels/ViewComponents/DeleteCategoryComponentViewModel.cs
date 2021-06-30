eadcf36b05f4c95d17e28fc1d17d968fa452fc5d
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-06-30 14:52:06 +0300

    delete category view component created

---
+++ Web/MoneySaver.Web.ViewModels/ViewComponents/DeleteCategoryComponentViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MoneySaver.Web.ViewModels.ViewComponents
+{
+    public class DeleteCategoryComponentViewModel
+    {
+        public int CategoryId { get; set; }
+        public int WalletId { get; set; }
+    }
+}
---
+++ Web/MoneySaver.Web/ViewComponents/DeleteCategoryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿namespace MoneySaver.Web.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+    using MoneySaver.Web.ViewModels.ViewComponents;
+
+    public class DeleteCategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int categoryId, int walletId)
+        {
+            DeleteCategoryComponentViewModel viewModel = new DeleteCategoryComponentViewModel();
+            viewModel.CategoryId = categoryId;
+            viewModel.WalletId = walletId;
+
+            return this.View(viewModel);
+        }
+    }
+}
