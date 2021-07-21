06bcadeedc5650a0dd7e99526298e998c404bee7
Vasil Dimitrov <v.b.dimitrow@gmai.com>
2021-07-21 16:59:57 +0300

    RAW scaffolded identity

---
+++ Web/MoneySaver.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MoneySaver.Data;
+using MoneySaver.Data.Models;
+
+[assembly: HostingStartup(typeof(MoneySaver.Web.Areas.Identity.IdentityHostingStartup))]
+namespace MoneySaver.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
